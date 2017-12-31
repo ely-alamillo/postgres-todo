de69e708c60339b38f4b0e47773dc749d3b034b7
Ely Alamillo <ely0013@gmail.com>
2017-12-31 12:10:52 -0600

    fixed pool connection and query

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const pg = require('pg');
+const { Pool } = require('pg');
 const path = require('path');
 const connectionString = 'postgres://xvdswdww:BGIa3B60dgLwtLGukxwoQeAclbHr253j@baasu.db.elephantsql.com:5432/xvdswdww';
+
+const pool = new Pool({
+    host: 'baasu.db.elephantsql.com',
+    user: 'xvdswdww',
+    password: 'BGIa3B60dgLwtLGukxwoQeAclbHr253j',
+    database: 'xvdswdww',
+    port: 5432,
+    max: 5,
+    idleTimeoutMillis: 20000,
+    connectionTimeoutMillis: 3000,
+});
+
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
+});
 
 const corsOptions = {
     "origin": true,
@@ -27,22 +45,16 @@ api.post('/todos', (req, res) => {
     const results = [];
     const { todo } = req.body;
     const todoObj = { todo, completed: false };
-    pg.connect(connectionString)
-        .then((client, done) => {
+    pool.connect()
+        .then((client) => {
+            
             // insert data
             client.query('INSERT INTO items(text, complete) values($1, $2)', [todoObj.todo, todoObj.completed]);
 
             // SQL Query > Select Data
             const query = client.query('SELECT * FROM items ORDER BY id ASC');
-            // Stream results back one row at a time
-            query.on('row', (row) => {
-            results.push(row);
-            // after all reading is done
-            })
-            query.on('end', () => {
-                done();
-                return res.json(results);
-            });
+            query
+                .then((data) => res.json(data))
         })
         .catch(err => console.log('error: ', err));
 });
